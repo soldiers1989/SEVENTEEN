6da73934d8d5af4ff625f085ab0918b2d66f6a1c
kitCooler <chenshaokangvip@163.com>
2018-04-11 17:55:59 +0800

    hello

---
+++ seventeen-service/src/main/java/com/seveteen/SeveteenApplication.java
@@ -0,0 +1,12 @@
+package com.seveteen;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeveteenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeveteenApplication.class, args);
+    }
+}
