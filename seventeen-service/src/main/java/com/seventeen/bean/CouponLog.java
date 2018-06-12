fcfdca0b62f652fab0ada8e8a4d29805950ef74b
kitCooler <chenshaokangvip@163.com>
2018-06-12 13:49:26 +0800

    a

---
+++ seventeen-service/src/main/java/com/seventeen/bean/CouponLog.java
@@ -0,0 +1,80 @@
+package com.seventeen.bean;
+
+/**
+ * @Author: csk
+ * @Date: 2018/6/6 16:58
+ */
+public class CouponLog {
+    private String userId;
+    private String userName;
+    private String couponId;
+    private String useStatus;
+    private String couponStatus;
+    private String useTime;
+    private String price;
+    private String maxPrice;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(String maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(String couponId) {
+        this.couponId = couponId;
+    }
+
+    public String getUseStatus() {
+        return useStatus;
+    }
+
+    public void setUseStatus(String useStatus) {
+        this.useStatus = useStatus;
+    }
+
+    public String getCouponStatus() {
+        return couponStatus;
+    }
+
+    public void setCouponStatus(String couponStatus) {
+        this.couponStatus = couponStatus;
+    }
+
+    public String getUseTime() {
+        return useTime;
+    }
+
+    public void setUseTime(String useTime) {
+        this.useTime = useTime;
+    }
+}
