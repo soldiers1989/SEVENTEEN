dacc87721a055bcaaf877031ba509f0c5d62371e
kitCooler <chenshaokangvip@163.com>
2018-07-26 17:40:36 +0800

    a

---
+++ seventeen-service/src/main/java/com/seventeen/bean/SeUserCoupon.java
@@ -0,0 +1,77 @@
+package com.seventeen.bean;
+
+
+import javax.persistence.Id;
+
+public class SeUserCoupon {
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String couponId;
+
+    private String status;
+
+    private String updateTime;
+
+    private String createTime;
+
+    private String createBy;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(String couponId) {
+        this.couponId = couponId == null ? null : couponId.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime == null ? null : updateTime.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+}
---
+++ seventeen-service/src/main/java/com/seventeen/mapper/SeUserCouponMapper.java
@@ -0,0 +1,9 @@
+package com.seventeen.mapper;
+
+import com.seventeen.bean.SeUserCoupon;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SeUserCouponMapper  extends CoreMapper<SeUserCoupon>{
+
+}
