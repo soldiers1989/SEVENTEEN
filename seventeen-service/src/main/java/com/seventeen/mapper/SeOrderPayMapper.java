6fcd0f39100ee3820c86df814e3bf6b1725dc66c
aa542393485 <542393485@qq.com>
2018-08-27 15:39:47 +0800

    ``

--- seventeen-service/src/main/java/com/seventeen/mapper/SeOrderPayMapper.java
+++ seventeen-service/src/main/java/com/seventeen/mapper/SeOrderPayMapper.java
@@ -1,9 +1,19 @@
 package com.seventeen.mapper;
 
 import com.seventeen.bean.SeOrderPay;
+import com.seventeen.bean.SeShop;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface SeOrderPayMapper extends CoreMapper<SeOrderPay>{
+
+    @Update("UPDATE `se_order_pay` a SET a.`status`=3,a.`out_pay_time`=NOW() WHERE a.id= #{orderId}")
+    void updateCancel(@Param("orderId") String orderPayId);
+
 
 }
