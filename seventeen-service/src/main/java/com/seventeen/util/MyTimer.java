e67a4a59c9b4cb44955d0fc4f3db721abce3f89c
kitCooler <chenshaokangvip@163.com>
2018-09-12 14:00:05 +0800

    a

--- seventeen-service/src/main/java/com/seventeen/util/MyTimer.java
+++ seventeen-service/src/main/java/com/seventeen/util/MyTimer.java
@@ -10,7 +10,7 @@ public class MyTimer extends Timer {
 
     public TimerTask schedule(final Runnable r, long delay) {
         final TimerTask task = new TimerTask() { public void run() { r.run(); }};
-        this.schedule(task, delay); 
+        this.schedule(task, delay);
         return task;
     }
 }
